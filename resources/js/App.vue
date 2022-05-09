65584c17aedb3c56b13b6b302260523bd7d21a18
Jeffrey van Reenen <jeffrey92.hrb@gmail.com>
2022-05-09 12:43:20 +0200

    Initial commit

---
+++ resources/js/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="mx-auto w-full">
+        <div class="flex flex-col justify-start w-full h-screen">
+            <transition name="fade">
+                <router-view></router-view>
+            </transition>
+        </div>
+    </div>
+</template>
+
+<style>
+
+</style>
+
---
+++ resources/js/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+    <Hero class="w-full"/>
+    <Blog class="w-full"/>
+    <Footer class="w-full"/>
+</template>
+
+<script>
+import Hero from '../components/Hero.vue';
+import Blog from '../components/Blog.vue';
+import Footer from '../components/Footer.vue';
+
+export default {
+    components: {
+        Hero, Blog, Footer
+    }
+}
+</script>
